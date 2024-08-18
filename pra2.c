8d5ddcf0419c53bb576dd0ef8e44a5658d635713
Pallavi Verulkar <verulkarpallavi1686@gmail.com>
2024-08-18 21:39:38 +0530

    Update pra2.c

--- pra2.c
+++ pra2.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 //program to calculate area and perimeter of rectangle
 // STUDENT ID:24TCEIOT
-void main(){
-    int length=50;
-    int width=30;
-    int area=length*width;
-    int perimeter=2*(length+width);
+void main()
+{
+    int length=50,width=30, area, perimeter;
+    area=length*width;
+    perimeter=2*(length+width);
     printf("area is:%d \n",area);
     printf("perimeter is: %d",perimeter);
 }
