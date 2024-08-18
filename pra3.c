28a8fb647c04010b0db4f6960aca5c4f68801ae9
Pallavi Verulkar <verulkarpallavi1686@gmail.com>
2024-08-18 21:41:43 +0530

    Update pra3.c

--- pra3.c
+++ pra3.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 //progmer to calculate simple interest
 //ID 24TCEIOT
-void main(){
-    int amount=50000;
-    int time=3;
-    float rate=5.5;
-    float interest=(amount*time*rate)/100;
+void main()
+{
+    int amount=50000,time=3;
+    float rate=5.5,interest;
+    interest=(amount*time*rate)/100;
     printf("simple interest is: %f",interest);
-
-
-
 }
