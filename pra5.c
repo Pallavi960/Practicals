e33a785b356244c4794690e4c51947f1f46fa044
Pallavi Verulkar <verulkarpallavi1686@gmail.com>
2024-08-21 22:23:26 +0530

    Update pra5.c

--- pra5.c
+++ pra5.c
@@ -1,28 +1,29 @@
-#include<stdio.h>
-int main(){
-int a,b;
-char n;
-printf("enter the number : ");
-scanf("%d", &a);
-printf("enter the number: ");
-scanf("%d", &b);
-printf(" enter'+'for addition\nenter '-' for substraction\nenter'*'for multiplication\nenter'/'for division\n");
-scanf(" %c",&n);
-switch(n){
-        case'+':
-               printf("ans is:%d",a+b);
-               break;
-        case'-':
-               printf("ans is:%d",a-b);
-               break;
-        case'*':
-               printf("ans is:%d",a*b);
-               break;
-        case'/':
-               printf("ans is:%d",a/b);
-               break;
-        deafult:
-               printf("invalid ");
-
-}
+#include <stdio.h>
+int main()
+{
+       int a, b;
+       char n;
+       printf("enter the number : ");
+       scanf("%d", &a);
+       printf("enter the number: ");
+       scanf("%d", &b);
+       printf(" enter'+'for addition\nenter '-' for substraction\nenter'*'for multiplication\nenter'/'for division\n");
+       scanf(" %c", &n);
+       switch (n)
+       {
+       case '+':
+              printf("ans is:%d", a + b);
+              break;
+       case '-':
+              printf("ans is:%d", a - b);
+              break;
+       case '*':
+              printf("ans is:%d", a * b);
+              break;
+       case '/':
+              printf("ans is:%d", a / b);
+              break;
+       deafult:
+              printf("invalid ");
+       }
 }
