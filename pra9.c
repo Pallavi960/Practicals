bb7d74753af70666638d2477937652c070be142c
Pallavi Verulkar <verulkarpallavi1686@gmail.com>
2024-08-22 11:15:35 +0530

    Update pra9.c

--- pra9.c
+++ pra9.c
@@ -2,7 +2,7 @@
 void main()
 {
  int marks;
- printf("enter the marks");
+ printf("enter the marks:");
  scanf("%d",&marks);
  marks>=90?printf("grade:A"):marks>=80?printf("grade:B"):marks>=70? printf("grade:C"):marks>=60? printf("grade:D"):marks<60?printf("grade:F"):printf("invalid marks");
 }
