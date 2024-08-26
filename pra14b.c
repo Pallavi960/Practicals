07efd1ba4b214cb6415bf9af0dfe71897265a337
Pallavi Verulkar <verulkarpallavi1686@gmail.com>
2024-08-26 16:47:07 +0530

    Add files via upload

---
+++ pra14b.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+    int i, j;
+    for (i = 1; i <= 5; i++)
+    {
+        int t = 1;
+        for (j = 1; j <= 5 - i; j++)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d", t);
+            t++;
+        }
+        printf("\n");
+    }
+}
