6eac6c703415a2d38b9099d37935522b71c7ba0e
Pallavi Verulkar <verulkarpallavi1686@gmail.com>
2024-08-26 16:50:37 +0530

    Update pra14c.c

--- practicle13.c
+++ practicle13.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 void main()
 {
-  int v, h, i, j;
-  printf("enter the size of table vertcally:");
-  scanf("%d", &v);
-  printf("enter the size of table horizontally:");
-  scanf("%d", &h);
-  for (i = 1; i <= v; i++)
-  {
-    for (j = 1; j <= h; j++)
-    {
-      printf("%3d", i * j);
-    }
-    printf("\n");
-  }
+   int i, j, n = 5;
+   for (i = 1; i <= n; i++)
+   {
+      for (j = 1; j <= n - i; j++)
+      {
+         printf(" ");
+      }
+      int t = 65;
+      for (j = n - i + 1; j <= n; j++)
+      {
+         printf("%s", &t);
+         t++;
+      }
+      t = t - 2;
+      for (j = 1; j <= i - 1; j++)
+      {
+         printf("%s", &t);
+         t--;
+      }
+      printf("\n");
+   }
 }
+
